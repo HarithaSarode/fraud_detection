9167fdf6649af621965bf68321459e8a3974a603
HarithaSarode <haritha.bits445@gmail.com>
2022-05-06 16:45:49 +0530

    added sum function and mumltiplication func

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -1,1 +1,16 @@
 print("all data preprocessing is done in this file")
+
+a = 5
+b = 7
+
+def sum(a,b):
+    c = a+b
+    return c
+print("sum function")
+
+def multiplication(a,b):
+    d = a*b
+    return b
+
+print("multiplication function")
+
