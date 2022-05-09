50bf0d18d8f7ee9f06fb5fb37a4dbad9332a236e
HarithaSarode <haritha.bits445@gmail.com>
2022-05-09 13:22:18 +0530

    added 3 new ml models

--- ml_model.py
+++ ml_model.py
@@ -1,1 +1,14 @@
+
 print("adding all ML related code here")
+
+def mdoel1():
+    print("adding code for ML model1")
+
+def model2():
+    print("addoing code for ML model2")
+
+def model3():
+    print("addoing code for ML model3")
+
+
+  
